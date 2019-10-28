a6f9ab128129393e39b6973b134f95ed8374a61b
fg-uulm <florian.geiselhart@uni-ulm.de>
2019-10-28 23:30:14 +0100

    Updated folder contents

---
+++ index.js
@@ -0,0 +1,39 @@
+/*
+ * CURRENCY CONVERTER RELOADED
+ * Author: <your name here>
+ * ---------------------------
+ *
+ * This converts currencies...somehow.
+ *
+ * A list of ressources you used, for example links:
+ * [JavaScript Reference](https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference).
+ */
+
+/*
+ *  Aufgabe: Baut einen neuen Währungsumrechner. Nachfolgend findet ihr Code der die 
+ *  dafür notwendingen Eingabewerte von der Konsole entgegennimmt.
+ * 
+ *  Dafür müsst ihr das Script wie folgt aufrufen:
+ *  npm start -- <Ausgangssumme> <Ausgangswährung-Code> <Zielwährung-Code>
+ *  also z.B.
+ *  npm start -- 10.0 USD EUR
+ * 
+ *  Die erwartete Ausgabe ist ein Text in folgender Form:
+ *  "Ergebnis: <Ausgangssumme> <Ausgangswährung> = <Ergebnis> <Zielwährung>"
+ *  also z.B.
+ *  Ergebnis: 10.00 USD = 11.00 EUR
+ *  
+ *  Das Script soll mindestens drei verschiedene Währungen in beide Richtungen unterstützen
+ */
+
+let args = process.argv.slice(2);
+
+let amount, originalCurrency, targetCurrency;
+
+if (args.length < 3) {
+  console.log('Error: Not enough input arguments given!');
+} else {
+  amount = args[0];
+  originalCurrency = args[1];
+  targetCurrency = args[2];
+}
